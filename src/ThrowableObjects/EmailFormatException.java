c33c0ea596a2a9aeeac11d8b8420c9c646af64e2
raduoprescu <r_oprescu@yahoo.com>
2023-02-23 18:08:12 +0200

    Initial commit

---
+++ src/ThrowableObjects/EmailFormatException.java
@@ -0,0 +1,18 @@
+package ThrowableObjects;
+
+import java.util.InputMismatchException;
+
+public class EmailFormatException extends InputMismatchException {
+    public EmailFormatException() {
+        super();
+    }
+
+    public EmailFormatException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String getMessage() {
+        return "Error, email must contain '@'! ";
+    }
+}
