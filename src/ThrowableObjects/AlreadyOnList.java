0046e6dd76de3c9f7e302860b7b884e73ffb9948
raduoprescu <r_oprescu@yahoo.com>
2023-02-23 18:06:44 +0200

    Initial commit

---
+++ src/ThrowableObjects/AlreadyOnList.java
@@ -0,0 +1,12 @@
+package ThrowableObjects;
+
+public class AlreadyOnList extends RuntimeException {
+    public AlreadyOnList() {
+        super();
+    }
+
+    @Override
+    public String getMessage() {
+        return "Error.Guest is already registered.";
+    }
+}
