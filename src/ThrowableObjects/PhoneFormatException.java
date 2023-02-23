12da400a50e5e74b3397d62b86909f218f99b4de
raduoprescu <r_oprescu@yahoo.com>
2023-02-23 18:12:51 +0200

    Initial commit

---
+++ src/ThrowableObjects/PhoneFormatException.java
@@ -0,0 +1,20 @@
+package ThrowableObjects;
+
+import java.util.InputMismatchException;
+
+
+public class PhoneFormatException extends InputMismatchException {
+    public PhoneFormatException() {
+        super();
+    }
+
+    public PhoneFormatException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String getMessage() {
+        return "Error, phone number must contain 10 digits!";
+    }
+}
+
